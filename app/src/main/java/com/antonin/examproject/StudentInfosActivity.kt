8a46872af46990db28366244b877f7fec898701a
Antonin SIMON <antonin.simon@epsi.fr>
2023-02-12 18:05:57 +0100

    add link and custom screen

--- app/src/main/java/com/antonin/examproject/StudentInfosActivity.kt
+++ app/src/main/java/com/antonin/examproject/StudentInfosActivity.kt
@@ -1,8 +1,13 @@
 package com.antonin.examproject
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.ImageView
+import android.widget.TextView
+import com.squareup.picasso.Picasso
 
 class StudentInfosActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,6 +22,17 @@ class StudentInfosActivity : BaseActivity() {
 
 
 
+        val nameTextView = findViewById<TextView>(R.id.textStudentName)
+        val imageStudent = findViewById<ImageView>(R.id.imageStudent)
+        val emailTextView = findViewById<TextView>(R.id.textStudentEmail)
+        val groupTextView = findViewById<TextView>(R.id.textStudentGroup)
+
+        nameTextView.text = lastname + " " + firstname
+        emailTextView.text = email
+        groupTextView.text = group
+
+        Picasso.get().load(urlImage).into(imageStudent);
+
         /*val url=intent.extras!!.getString("url")
         val image=findViewById<ImageView>(R.id.imageViewDetails)
         Picasso.get().load(url).into(image);
@@ -24,5 +40,13 @@ class StudentInfosActivity : BaseActivity() {
 
         setHeaderTitle(firstname)
         showBack()
+
+        val linkToClick = findViewById<TextView>(R.id.textLink)
+
+        linkToClick.setOnClickListener(View.OnClickListener {
+            val openURL = Intent(android.content.Intent.ACTION_VIEW)
+            openURL.data = Uri.parse("https://www.epsi.fr/campus/campus-de-bordeaux/")
+            startActivity(openURL)
+        })
     }
 }
